e139208636a7f23d6435dd8360de7f292fbd407c
webspeakIT <41650593+webspeakIT@users.noreply.github.com>
2024-05-15 18:50:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,386 @@
+<?php include('tpl/header.php') ?>
+<!-- START CONTENT -->
+	<section class="content">
+		<div class="section section-main">
+			<div class="container">
+				<div class="text">
+					<div class="title">
+						<p>30 денний марафон медитацій</p>
+						<h1>Жіноче пробудження</h1>
+					</div>
+					<a href="#" class="btn">Отримати курс</a>
+					<p class="sale">
+						Прийми участь зі знижкою 90%
+						<span>
+							і отримай
+							<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M6.00769 0.059865C4.53066 0.356612 3.59773 1.48708 3.50304 3.0949C3.46427 3.75267 3.54358 4.15355 3.81604 4.67749C4.68292 6.34449 7.59726 7.0174 11.0668 6.35157C12.6469 6.04837 13.4912 5.60356 13.862 4.87884C14.0282 4.55403 14.0462 4.47303 14.0182 4.17583C13.9747 3.71458 13.7456 3.32657 13.2098 2.8066C12.0129 1.64523 9.74294 0.448158 8.11177 0.118251C7.46592 -0.0123518 6.50052 -0.0391639 6.00769 0.059865ZM21.2516 0.0608854C20.6049 0.170401 20.3443 0.242561 19.6607 0.501215C18.8336 0.814117 18.0354 1.21908 17.2294 1.73457C15.3571 2.93221 14.7243 3.88073 15.1551 4.84409C15.5471 5.72073 17.0133 6.31938 19.3366 6.5515C21.0053 6.71821 22.7072 6.49964 23.8017 5.97802C25.0548 5.38084 25.7111 4.22004 25.5355 2.9113C25.3353 1.41917 24.5514 0.455924 23.255 0.109238C22.8154 -0.00838386 21.8044 -0.0327585 21.2516 0.0608854ZM7.40163 2.21452C7.8455 2.25726 8.64591 2.5141 9.25818 2.81023C10.1852 3.25861 11.3101 3.98038 11.2187 4.06824C11.1233 4.16007 10.0473 4.33795 9.27703 4.38919C7.5005 4.50749 6.10685 4.20542 5.81978 3.63981C5.71932 3.44181 5.71513 3.38212 5.77871 3.05023C5.86285 2.61109 5.97363 2.38656 6.15317 2.29145C6.29977 2.21373 6.75082 2.14066 6.93331 2.16504C6.99812 2.17371 7.20889 2.19593 7.40163 2.21452ZM22.6416 2.21333C23.0283 2.2737 23.2845 2.7104 23.2845 3.30923C23.2845 3.60104 23.2683 3.63743 23.0491 3.83804C22.569 4.27741 21.3446 4.48499 19.7899 4.39072C19.156 4.35229 17.8859 4.15565 17.8119 4.08451C17.7597 4.03417 17.9676 3.86576 18.5138 3.5159C19.6282 2.80223 20.8362 2.30732 21.7401 2.19412C22.1287 2.14542 22.219 2.14735 22.6416 2.21333ZM0.85946 7.73724C0.530552 7.88094 0.21272 8.21085 0.102004 8.52335C-0.0303372 8.89702 -0.0355224 13.2736 0.0959349 13.6321C0.225212 13.9846 0.456426 14.2399 0.792229 14.4008L1.09362 14.5453H7.08091C12.1588 14.5453 13.0856 14.5331 13.1826 14.4649C13.2936 14.3868 13.297 14.2859 13.297 11.1154V7.84642L13.1579 7.73809C13.023 7.63294 12.8402 7.62977 7.05935 7.63096C1.38435 7.63215 1.08844 7.63719 0.85946 7.73724ZM15.8055 7.77907C15.6853 7.92612 15.6834 7.97736 15.6834 11.0875C15.6834 14.1976 15.6853 14.2489 15.8055 14.3959L15.9276 14.5453H21.9219H27.9162L28.2176 14.4008C28.3834 14.3213 28.5885 14.1687 28.6734 14.0616C28.9921 13.6597 29 13.5876 29 11.0905C29 8.51099 28.9902 8.43606 28.6033 8.05275C28.1471 7.60097 28.6449 7.63237 21.9188 7.63096L15.9276 7.62971L15.8055 7.77907ZM2.64448 16.1559C2.2765 16.2517 1.96126 16.5026 1.77778 16.8456L1.63023 17.1215V22.5647V28.008L1.75739 28.2348C1.92391 28.5316 2.15771 28.7397 2.48462 28.882C2.74488 28.9954 2.84582 28.9975 7.94838 28.9988L13.147 29L13.2375 28.8671C13.3177 28.7493 13.3262 28.0328 13.3125 22.5483L13.297 16.3623L13.1628 16.2333L13.0286 16.1041L7.97754 16.0933C3.99794 16.0848 2.86673 16.0981 2.64448 16.1559ZM15.9792 16.1486C15.6749 16.2967 15.6852 16.0702 15.6843 22.6371C15.6834 28.6983 15.6848 28.7746 15.8012 28.8866C15.9173 28.9983 15.9967 29 21.0896 28.9988C26.1639 28.9975 26.265 28.9954 26.5252 28.882C26.8521 28.7397 27.0859 28.5316 27.2525 28.2348L27.3796 28.008V22.5647V17.1215L27.2291 16.8401C27.0407 16.4878 26.6801 16.2184 26.2892 16.1377C25.8309 16.0431 16.1748 16.0533 15.9792 16.1486Z" fill="url(#paint0_linear_703_2025)" />
+								<defs>
+									<linearGradient id="paint0_linear_703_2025" x1="29" y1="-2.87138e-06" x2="0.385929" y2="2.58013" gradientUnits="userSpaceOnUse">
+									<stop stop-color="#5D30FF" />
+									<stop offset="1" stop-color="#9E96FA" />
+									</linearGradient>
+								</defs>
+							</svg>
+						</span>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="section section-for">
+			<div class="container">
+				<p class="section-title">Для кого цей марафон?</p>
+				<div class="row">
+					<div class="col-xl-4">
+						<div class="for-item">
+							<ul>
+								<li>Якщо ти-жертва насилля</li>
+								<li>Маєш проблеми з жіночим здоров’ям</li>
+								<li>Хочеш відпустити минулі відносини та травми</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="for-item">
+							<ul>
+								<li>Намірена звільнитись від комплексів та внутрішніх обмежень</li>
+								<li>Бажаєш збільшити фінансовий потік</li>
+								<li>Хочеш відчути себе сексуальною та бажаною</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="for-item">
+							<ul>
+								<li>Мрієш навчитись програмувати своє життя</li>
+								<li>Прагнеш змінити відносини з батьками</li>
+								<li>Хочеш трансформувати життя та жити в гармонії</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="section section-author">
+			<div class="container">
+				<div class="text">
+					<p class="section-title">Про автора:</p>
+					<div class="description">
+						<span>Привіт мене звати Ніна</span>
+						<p>Я дипломований магістр психології та сексолог. Фахівець з роботи з жертвами насилля та експерт з кататимно-імагінативної психотерапії. За свою практику провела 100+ особистих консультацій та пройшла 200+ годин курсів підвищення кваліфікації. Я створила цей онлайн марафон для жінок за доступною ціною,тому що відчуваю що зараз це потрібно кожній.<br> Я вірю,що кожна жінка має необмежений потенціал і здатність до досягнення великих речей,але іноді нам потрібна підтримка щоб це реалізувати. </p>
+						<small>І саме цей курс-це моя підтримка для вас.</small>
+					</div>
+					<a href="#" class="btn">Отримати курс</a>
+				</div>
+				<div class="image">
+					<img src="img/author-img.jpg" alt="">
+				</div>
+			</div>
+		</div>
+		<div class="section section-recieve">
+			<div class="container">
+				<p class="section-title">Що ви отримуєте під час марафону?</p>
+				<div class="row">
+					<div class="col-xl-4">
+						<div class="recieve-item">
+							<div class="recieve-img">
+								<img src="img/recieve-img1.png" alt="">
+							</div>
+							<p>30 медитацій від 7 до 15 хвилин</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="recieve-item">
+							<div class="recieve-img">
+								<img src="img/recieve-img2.png" alt="">
+							</div>
+							<p>Чек-лист 60 способів відкриття жіночності</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="recieve-item">
+							<div class="recieve-img">
+								<img src="img/recieve-img3.png" alt="">
+							</div>
+							<p>Психологічні практики</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="recieve-item">
+							<div class="recieve-img">
+								<img src="img/recieve-img4.png" alt="">
+							</div>
+							<p>Медитації 18+</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="recieve-item">
+							<div class="recieve-img">
+								<img src="img/recieve-img5.png" alt="">
+							</div>
+							<p>3 додаткових медитації 728Hz</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="recieve-item">
+							<div class="recieve-img">
+								<img src="img/recieve-img6.png" alt="">
+							</div>
+							<p>Доступ назавжди</p>
+						</div>
+					</div>
+				</div>
+				<div class="button-with-text">
+					<p><span>Зараз</span> – найкращий час для змін!</p>
+					<a href="#" class="btn">
+						Отримати курс
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="section section-program">
+			<div class="container">
+				<p class="section-title">Програма марафону:</p>
+				<div class="row">
+					<div class="col-xl-4">
+						<div class="program-item">
+							<div class="program-title">
+								<svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M13.9682 2.06904C14.6125 0.327799 17.0753 0.327801 17.7196 2.06904L20.6143 9.89192C20.8169 10.4394 21.2485 10.871 21.7959 11.0736L29.6188 13.9683C31.36 14.6126 31.36 17.0754 29.6188 17.7197L21.7959 20.6144C21.2485 20.817 20.8169 21.2486 20.6143 21.7961L17.7196 29.6189C17.0753 31.3602 14.6125 31.3602 13.9682 29.6189L11.0735 21.7961C10.8709 21.2486 10.4393 20.817 9.89185 20.6144L2.06898 17.7197C0.327748 17.0754 0.32775 14.6126 2.06898 13.9683L9.89185 11.0736C10.4393 10.871 10.8709 10.4394 11.0735 9.89192L13.9682 2.06904Z" fill="url(#paint0_linear_364_10200)"/>
+									<defs>
+									<linearGradient id="paint0_linear_364_10200" x1="15.9046" y1="0.140666" x2="15.9046" y2="35.7349" gradientUnits="userSpaceOnUse">
+										<stop stop-color="#F66EFF"/>
+										<stop offset="1" stop-color="#04D2FF"/>
+									</linearGradient>
+									</defs>
+								</svg>
+								<p>Робота з травмами</p>
+							</div>
+							<ul>
+								<li>Медитація очищення і відпускання образ</li>
+								<li>Божественний храм зцілення</li>
+								<li>Водоспад</li>
+								<li>Медитація зцілення внутрішньої дитини</li>
+								<li>Відпускання минулого</li>
+								<li>Трансформація відносин з мамою</li>
+								<li>Трансформація відносин з батьком</li>
+								<li>Розмова з маткою</li>
+								<li>Завершення кармічних відносин</li>
+								<li>Прощення себе</li>
+							</ul>
+							<div class="program-img">
+								<img src="img/program-img1.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="program-item">
+							<div class="program-title">
+								<svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M13.9682 2.06904C14.6125 0.327799 17.0753 0.327801 17.7196 2.06904L20.6143 9.89192C20.8169 10.4394 21.2485 10.871 21.7959 11.0736L29.6188 13.9683C31.36 14.6126 31.36 17.0754 29.6188 17.7197L21.7959 20.6144C21.2485 20.817 20.8169 21.2486 20.6143 21.7961L17.7196 29.6189C17.0753 31.3602 14.6125 31.3602 13.9682 29.6189L11.0735 21.7961C10.8709 21.2486 10.4393 20.817 9.89185 20.6144L2.06898 17.7197C0.327748 17.0754 0.32775 14.6126 2.06898 13.9683L9.89185 11.0736C10.4393 10.871 10.8709 10.4394 11.0735 9.89192L13.9682 2.06904Z" fill="url(#paint0_linear_364_10200)"/>
+									<defs>
+									<linearGradient id="paint0_linear_364_10200" x1="15.9046" y1="0.140666" x2="15.9046" y2="35.7349" gradientUnits="userSpaceOnUse">
+										<stop stop-color="#F66EFF"/>
+										<stop offset="1" stop-color="#04D2FF"/>
+									</linearGradient>
+									</defs>
+								</svg>
+								<p>Контакт з собою</p>
+							</div>
+							<ul>
+								<li>Зустріч з душею</li>
+								<li>Медитація з ангелом</li>
+								<li>Медитація сила роду</li>
+								<li>Медитація вдячності</li>
+								<li>Квітучий сад</li>
+								<li>Розширення фінансового потоку</li>
+								<li>Всесвіт всередині</li>
+								<li>Медитація для здійснення бажань</li>
+								<li>Медитація самопрограмування</li>
+								<li>Зцілення землі</li>
+							</ul>
+							<div class="program-img">
+								<img src="img/program-img2.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="program-item">
+							<div class="program-title">
+								<svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M13.9682 2.06904C14.6125 0.327799 17.0753 0.327801 17.7196 2.06904L20.6143 9.89192C20.8169 10.4394 21.2485 10.871 21.7959 11.0736L29.6188 13.9683C31.36 14.6126 31.36 17.0754 29.6188 17.7197L21.7959 20.6144C21.2485 20.817 20.8169 21.2486 20.6143 21.7961L17.7196 29.6189C17.0753 31.3602 14.6125 31.3602 13.9682 29.6189L11.0735 21.7961C10.8709 21.2486 10.4393 20.817 9.89185 20.6144L2.06898 17.7197C0.327748 17.0754 0.32775 14.6126 2.06898 13.9683L9.89185 11.0736C10.4393 10.871 10.8709 10.4394 11.0735 9.89192L13.9682 2.06904Z" fill="url(#paint0_linear_364_10200)"/>
+									<defs>
+									<linearGradient id="paint0_linear_364_10200" x1="15.9046" y1="0.140666" x2="15.9046" y2="35.7349" gradientUnits="userSpaceOnUse">
+										<stop stop-color="#F66EFF"/>
+										<stop offset="1" stop-color="#04D2FF"/>
+									</linearGradient>
+									</defs>
+								</svg>
+								<p>Сексуальність 18+</p>
+							</div>
+							<ul>
+								<li>Сексуальний гіпноз</li>
+								<li>Медитація Тотем</li>
+								<li>Практика енергія вогню</li>
+								<li>Медитація оргазмічності</li>
+								<li>Медитація внутрішній магнетизм</li>
+								<li>Секс з собою</li>
+								<li>Практика чуттєвості</li>
+								<li>Прийняття тіла</li>
+								<li>Дихання маткою</li>
+								<li>Практика енергія вогню</li>
+							</ul>
+							<div class="program-img">
+								<img src="img/program-img3.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="button-with-text">
+					<p>Побудуй <span>гармонічні відносини</span> з собою за місяць!</p>
+					<a href="#" class="btn">
+						Отримати курс
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="section section-steps">
+			<div class="container">
+				<p class="section-title">Як буде проходити марафон?</p>
+				<div class="row">
+					<div class="col-xl-6">
+						<div class="step-item">
+							<div class="step-image">
+								<img src="img/step-img1.png" alt="">
+							</div>
+							<p>Після оплати надається доступ до навчальної платформи на твою пошту </p>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<div class="step-item">
+							<div class="step-image">
+								<img src="img/step-img2.png" alt="">
+							</div>
+							<p>Онлайн курс триватиме 30 днів</p>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<div class="step-item">
+							<div class="step-image">
+								<img src="img/step-img3.png" alt="">
+							</div>
+							<p>Старт одразу після оплати</p>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<div class="step-item">
+							<div class="step-image">
+								<img src="img/step-img4.png" alt="">
+							</div>
+							<p>Курс можна проходити у будь який час </p>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<div class="step-item">
+							<div class="step-image">
+								<img src="img/step-img5.png" alt="">
+							</div>
+							<p>Ми рекомендуємо проходити по одному уроку щодня</p>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<div class="step-item">
+							<div class="step-image">
+								<img src="img/step-img6.png" alt="">
+							</div>
+							<p>Доступ до марафону у тебе буде назавжди, що дасть можливість проходити курс, навіть якщо ти пропустиш якийсь із днів.</p>
+						</div>
+					</div>
+				</div>
+				<div class="button-with-text">
+					<p>Якщо ти тут, то <span>перший крок</span> до змін уже зроблено!</p>
+					<a href="#" class="btn">
+						Отримати курс
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="section section-get">
+			<div class="container">
+				<p class="section-title">Що ти отримуєш пройшовши<br> онлайн марафон?</p>
+				<div class="row">
+					<div class="col-xl-4">
+						<div class="get-item">
+							<div class="get-img">
+								<img src="img/get-img1.png" alt="">
+							</div>
+							<p>Відпустиш минуле</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="get-item">
+							<div class="get-img">
+								<img src="img/get-img2.png" alt="">
+							</div>
+							<p>Пройдеш всі травмуючі моменти та почнеш нове життя</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="get-item">
+							<div class="get-img">
+								<img src="img/get-img3.png" alt="">
+							</div>
+							<p>Трансформуєш відносини з батьками</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="get-item">
+							<div class="get-img">
+								<img src="img/get-img4.png" alt="">
+							</div>
+							<p>Приймеш своє тіло та бажання</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="get-item">
+							<div class="get-img">
+								<img src="img/get-img5.png" alt="">
+							</div>
+							<p>Покращиш фінансовий стан</p>
+						</div>
+					</div>
+					<div class="col-xl-4">
+						<div class="get-item">
+							<div class="get-img">
+								<img src="img/get-img6.png" alt="">
+							</div>
+							<p>Розкриєш сексуальність та забудеш про комплекси та внутрішні обмеження</p>
+						</div>
+					</div>
+				</div>
+				<div class="button-with-text">
+					<p><span>10 хвилин на день</span> і ти зміниш своє життя вже за місяць</p>
+					<a href="#" class="btn">
+						Отримати курс
+					</a>
+				</div>
+				<img src="img/get-image.png" class="get-image" alt="">
+			</div>
+		</div>
+		<div class="section section-cta">
+			<div class="container">
+				<div class="cta-wrapper">
+					<img src="img/cta-image.png" class="top-image" alt="">
+					<p class="title">ЗМІНИ ВСЕ!</p>
+					<div class="description">
+						Ти відчуваєш,що це саме той момент коли потрібно щось змінити? Готова розкрити свою внутрішню жіночу силу та стати жінкою якою завжди мріяла?
+					</div>
+					<p class="price">всього лише за <span>333 грн</span> замість <small>3333грн</small></p>
+					<a href="#" class="btn">ЦЕ ТВІЙ ШАНС!</a>
+					<small>На 100% без ризику для тебе</small>
+				</div>
+			</div>
+		</div>
+	</section>
+<!-- END CONTENT -->
+<?php include('tpl/footer.php') ?>
